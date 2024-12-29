d6ec285cd94baf15091afe7b1d361136b9a15e5a
mirak2010 <mirakhmad0101@yahoo.com>
2024-12-29 10:19:44 +0500

    #

---
+++ average.py
@@ -0,0 +1,17 @@
+a=98
+b=95
+c=41
+d=65
+e=94
+result=((a+b+c+d+e)/5)
+print(result,"%")
+
+math=int(input("Enter your math grade:"))
+sc=int(input("Enter your science grade:"))
+physics=int(input("Enter your physics grade:"))
+chem=int(input("Enter your chemistry grade:"))
+eng=int(input("Enter your english grade:"))
+print("Here is your percentage:")
+result=((math+sc+physics+chem+eng)/5)
+print(result, "%")
+
